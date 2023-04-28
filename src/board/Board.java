bac72b72fed0f35f0e7d755957a9989428499f3c
mhmFrost <tim.thieler@brightstraining.com>
2023-04-28 09:18:31 +0200

    add rents on board

--- src/board/Board.java
+++ src/board/Board.java
@@ -77,9 +77,29 @@ public class Board {
         };
 
         int[][] rents = {
-                {2, 10, 30, 90, 160, 250},
-                {4, 20, 60, 180, 320, 450},
-                // TODO: Add remaining rents from table here
+                {2, 10, 30, 90, 160, 250},          // Mediterranean Avenue, Brown
+                {4, 20, 60, 180, 320, 450},         // Baltic Avenue, Brown
+                {6, 30, 90, 270, 400, 550},         // Oriental Avenue, Lightblue
+                {6, 30, 90, 270, 400, 550},         // Vermont Avenue, Lightblue
+                {8, 40, 100, 300, 450, 600},        // Connecticut Avenue, Lightblue
+                {10, 50, 150, 450, 625, 750},       // St. Charles Place, Pink
+                {10, 50, 150, 450, 625, 750},       // States Avenue, Pink
+                {12, 60, 180, 500, 700, 900},       // Virginia Avenue, Pink
+                {14, 70, 200, 550, 750, 950},       // St. James Place, Orange
+                {14, 70, 200, 550, 750, 950},       // Tennessee Avenue, Orange
+                {16, 80, 220, 600, 800, 1000},      // New York Avenue, Orange
+                {18, 90, 250, 700, 875, 1050},      // Kentucky Avenue, Red
+                {18, 90, 250, 700, 875, 1050},      // Indiana Avenue, Red
+                {20, 100, 300, 750, 925, 1100},     // Illinois Avenue, Red
+                {22, 110, 330, 800, 975, 1150},     // Atlantic Avenue, Yellow
+                {22, 110, 330, 800, 975, 1150},     // Ventnor Avenue, Yellow
+                {22, 120, 360, 850, 1025, 1200},    // Marvin Gardens, Yellow
+                {26, 130, 390, 900, 1100, 1275},    // Pacific Avenue, Green
+                {26, 130, 390, 900, 1100, 1275},    // North Carolina Avenue, Green
+                {28, 150, 450, 1000, 1200, 1400},   // Pennsylvania Avenue, Green
+                {35, 175, 500, 1100, 1300, 1500},   // Park Place, Blue
+                {50, 200, 600, 1400, 1700, 2000},   // Boardwalk, Blue
+                // TODO: check rents
                 // {Site,1 hse,	2 hses,	3 hses,	4 hses,	Hotel}
                 // https://www.jdawiseman.com/papers/trivia/monopoly-rents.html
         };
